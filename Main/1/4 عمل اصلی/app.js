2429e84680f41a217a6215e0ccc1af72efad0dac
Amirhosein-st <110676193+Amirhosein-st@users.noreply.github.com>
2023-01-29 09:19:21 -0800

    ex 1

---
+++ Main/1/4 عمل اصلی/app.js
@@ -0,0 +1,22 @@
+console.log('Hello World');
+
+function sum(num1 , num2) {
+    return num1 + num2
+}
+
+function sub(num1 , num2) {
+    return num1 - num2
+}
+
+function mul(num1 , num2) {
+    return num1 * num2
+}
+
+function div(num1 , num2) {
+    return num1 / num2
+}
+
+ console.log(`4 + 10 = ${sum(4 , 10)}`);
+ console.log(`10 - 4 = ${sub(10 , 4)}`);
+ console.log(`4 * 10 = ${mul(4 , 10)}`);
+ console.log(`10 / 5 = ${div(10 , 5)}`);
